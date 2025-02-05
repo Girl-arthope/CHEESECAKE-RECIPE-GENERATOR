0ef9bf0040946e569cd2d1e8613e6c6f89d9cd5e
Girl-arthope <chidemetariro@gmail.com>
2025-02-05 06:51:05 +0200

    updated recipe context

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 function displayRecipe(response) {
- 
-
- 
   if (response.data && response.data.answer) {
     new Typewriter("#recipe", {
       strings: response.data.answer,
@@ -21,20 +18,17 @@ function generateRecipe(event) {
 
   let apiKey = "o26te0f2d5dba0a4524b73ca31ad79a0";
   let context =
-    "You are a professional recipe expert and love to write creative recipes. Your mission is to generate a 10-line recipe in basic HTML, separating each step with <br />. Make sure to follow the user instructions. Do not include a title in the recipe.Ressults can appear in bullet point form. Sign the recipe with 'girl_arthope ai' in pink inside a <strong> element at the end of the recipe and NOT at the beginning.";
+    "You are a professional cheesecake recipe expert and love to write creative recipes. Your mission is to generate a 10-line cheesecake recipe in basic HTML, separating each step with <br />. Make sure to follow the user instructions. Do not include a title in the recipe.Results can appear in bullet point form.Stop including this '```html' in the beginning of every recipe you generate. Sign the recipe with 'girl_arthope ai' in pink inside a <strong> element at the end of the recipe and NOT at the beginning.";
 
   let prompt = `User instructions: Generate a recipe for a ${instructionsInput.value} dish.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-
   console.log("API URL:", apiUrl);
 
-  
   let recipeElement = document.querySelector("#recipe");
   recipeElement.classList.remove("hidden");
   recipeElement.innerHTML = `<div class="generating">⏳ Generating a recipe for ${instructionsInput.value}...</div>`;
 
-  
   axios
     .get(apiUrl)
     .then(displayRecipe)
